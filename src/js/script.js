df13189f52a0f5abf0c49d4ca37b924c0ecae286
AchabHanna <hanna.achab@hetic.net>
2020-01-08 16:04:03 +0100

    add js file game2

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,16 @@
 oxo.screens.loadScreen("home", function() {
   let play = document.querySelector(".play");
   play.addEventListener("click", function() {
-    oxo.screens.loadScreen("game0", function() {});
+    oxo.screens.loadScreen("game2", function() {});
   });
+});
+
+let daisyHere = document.getElementById("daisyhere");
+daisyHere.addEventListener("click", function() {
+  let daisy = document.getElementById("sleepingdaisy");
+  if (daisy.style.display == "none") {
+    daisy.style.display == "block";
+  } else {
+    daisy.style.display = "none";
+  }
 });
